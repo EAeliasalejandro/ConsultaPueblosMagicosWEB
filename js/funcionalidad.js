a11c81b9ac1035b376f91bf89f87def420b0bec3
Elias Alejandro <eaeliasalejandro@hotmail.com>
2022-05-03 22:55:18 -0500

    Aplicación web y Manejo de errores terminado.

--- js/funcionalidad.js
+++ js/funcionalidad.js
@@ -18,21 +18,47 @@ function obtenerDireccion() {
         .then((respuesta) => {
             return respuesta.json();
         }).then((respuesta) => {
-            let direccion = respuesta[0].split(",");
-            var lat = parseFloat(direccion[0]);
-            var lng = parseFloat(direccion[1]);
-            console.log(lat +" "+lng);
-
-            var map = new google.maps.Map(document.getElementById('map'), {
-                center: new google.maps.LatLng(lat, lng),
-                zoom: 8,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            });
-            var marker = new google.maps.Marker({
-                position: { lat: lat, lng: lng },
-                map: map,
-                title: document.getElementById('entrada2').value
-            });
-
+            // Eliminando todos los hijos de un elemento
+            var element = document.getElementById("error");
+            var element2 = document.getElementById("map");
+            while (element2.firstChild) {
+                element2.removeChild(element2.firstChild);
+            }
+            while (element.firstChild) {
+                element.removeChild(element.firstChild);
+            }
+            if (respuesta[0] == 'Selecciona el Pueblo Mágico' || respuesta[0] == 'No se encontró el pueblo mágico solicitado') {
+                var contenedor = document.createElement("div"); //div
+                contenedor.className = "form-floating"; //mapa class:form-floating nombre:contenedor
+                contenedor.id = "contenedor";
+                var txta = document.createElement("textarea");
+                txta.id = "mensajeError";
+                txta.className = "form-control";
+                txta.style = "height: 50px";
+                txta.readOnly = true;
+                txta.innerHTML = respuesta[0];
+                document.getElementById("error").appendChild(contenedor);
+                contenedor.appendChild(txta);
+            } else {
+                timeout = setTimeout(generarMapa(respuesta), 3000);
+            }/*
+            */
         })
+}
+function generarMapa(respuesta) {
+    let direccion = respuesta[0].split(",");
+    var lat = parseFloat(direccion[0]);
+    var lng = parseFloat(direccion[1]);
+    console.log(lat + " " + lng);
+    document.getElementById('map')
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: new google.maps.LatLng(lat, lng),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    var marker = new google.maps.Marker({
+        position: { lat: lat, lng: lng },
+        map: map,
+        title: document.getElementById('entrada2').value
+    });
 }
